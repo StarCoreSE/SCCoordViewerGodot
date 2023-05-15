363db4e54bcc1fd8e1dda94c5ad8acab23606e61
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-05-15 00:02:53 -0500

    multiple live updating instances almost working. just need to make it add new when it detecs

--- Scripts/MainDisplay.gd
+++ Scripts/MainDisplay.gd
@@ -1,39 +1,44 @@
 extends Node2D
 
 var entity = preload("res://Entities/Square.tscn") # Load the Square scene
-var file_path = "" # Initialize file path to an empty string
-var coords = Vector2.ZERO # Set the initial coordinates to (0,0)
 
 func _ready():
-	get_file_path() # Get the path to the text file
-	read_coordinates() # Read the initial coordinates from the text file
-	if coords != Vector2.ZERO: # Check if coordinates were successfully read
-		var square = entity.instance() # Instantiate the Square scene
-		add_child(square) # Add the Square as a child of this Node2D
-		square.position = coords # Set the position of the Square to the current coordinates
+	var dir = Directory.new()
+	dir.open("res://")
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if file_name.ends_with(".txt"):
+			var coords = read_coordinates("res://" + file_name)
+			if coords != Vector2.ZERO:
+				var square = entity.instance() # Instantiate the Square scene
+				add_child(square) # Add the Square as a child of this Node2D
+				square.position = coords # Set the position of the Square to the current coordinates
+				square.name = file_name.substr(0, file_name.length() - 4) # Set the name of the Square to the file name (without the .txt extension)
+		file_name = dir.get_next()
+	dir.list_dir_end()
 
 var timer = 1.0 # Set the timer to 1 second
 func _process(delta):
 	timer -= delta # Decrement the timer every frame
 	if timer <= 0.0:
-		read_coordinates() # Read the latest coordinates from the text file
-		if coords != Vector2.ZERO: # Check if coordinates were successfully read
-			get_node("Square").position = coords # Update the position of the Square to the current coordinates
+		var dir = Directory.new()
+		dir.open("res://")
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if file_name.ends_with(".txt"):
+				var coords = read_coordinates("res://" + file_name)
+				if coords != Vector2.ZERO:
+					var square = get_node(file_name.substr(0, file_name.length() - 4)) # Find the node with the same name as the file (without the .txt extension)
+					if square != null:
+						square.position = coords # Update the position of the Square to the current coordinates
+			file_name = dir.get_next()
+		dir.list_dir_end()
 		timer = 1.0 # Reset the timer to 1 second
 
-func get_file_path():
-	var dir = Directory.new()
-	dir.open(".")
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		if file_name.ends_with(".txt"): # Check if the file has a .txt extension
-			file_path = "res://" + file_name # Set the file path to the first .txt file found
-			break
-		file_name = dir.get_next()
-	dir.list_dir_end()
-
-func read_coordinates():
+func read_coordinates(file_path):
+	var coords = Vector2.ZERO # Set the initial coordinates to (0,0)
 	var file = File.new()
 	if file.file_exists(file_path):
 		file.open(file_path, File.READ)
@@ -48,5 +53,4 @@ func read_coordinates():
 				coords = Vector2(x_coord, y_coord) # Update coords
 			line = file.get_line().strip_edges()
 		file.close()
-	else:
-		coords = Vector2.ZERO # Set coords to (0,0) if file doesn't exist or can't be read
+	return coords # Return the coordinates read from the file
