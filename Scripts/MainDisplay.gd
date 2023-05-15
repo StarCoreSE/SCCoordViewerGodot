9f4b1e2f71d91a382f3e609abbbfc8e984032b87
InvalidArgument3 <51190031+InvalidArgument3@users.noreply.github.com>
2023-05-14 23:36:53 -0500

    pain

--- Scripts/MainDisplay.gd
+++ Scripts/MainDisplay.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 var entity = preload("res://Entities/Square.tscn") # Load the Square scene
-var file_path = "res://coordinates.txt" # Set the file path to the text file
+var file_path = "" # Initialize file path to an empty string
 var coords = Vector2.ZERO # Set the initial coordinates to (0,0)
 
 func _ready():
+	get_file_path() # Get the path to the text file
 	read_coordinates() # Read the initial coordinates from the text file
 	if coords != Vector2.ZERO: # Check if coordinates were successfully read
 		var square = entity.instance() # Instantiate the Square scene
@@ -19,6 +20,18 @@ func _process(delta):
 		if coords != Vector2.ZERO: # Check if coordinates were successfully read
 			get_node("Square").position = coords # Update the position of the Square to the current coordinates
 		timer = 1.0 # Reset the timer to 1 second
+
+func get_file_path():
+	var dir = Directory.new()
+	dir.open(".")
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	while file_name != "":
+		if file_name.ends_with(".txt"): # Check if the file has a .txt extension
+			file_path = "res://" + file_name # Set the file path to the first .txt file found
+			break
+		file_name = dir.get_next()
+	dir.list_dir_end()
 
 func read_coordinates():
 	var file = File.new()
